de1694c918eeea3b8622d6496f2cd82b878834bb
lizebin <lizebin0918@gmail.com>
2021-10-13 12:53:05 +0800

    feat:备忘录模式

---
+++ src/main/java/com/lzb/memento/v1/InputText.java
@@ -0,0 +1,31 @@
+package com.lzb.memento.v1;
+
+/**
+ * 备忘录模式<br/>
+ * Created on : 2021-10-13 10:02
+ *
+ * @author lizebin
+ */
+public class InputText {
+
+    private final StringBuilder text = new StringBuilder();
+
+    public String getText() {
+        return text.toString();
+    }
+
+    public void append(String input) {
+        text.append(input);
+    }
+
+    public void setText(String text) {
+        // 相当于把原字符串清空，替换成新的字符串
+        this.text.replace(0, this.text.length(), text);
+    }
+
+    @Override
+    public String toString() {
+        return this.text.toString();
+    }
+
+}
---
+++ src/main/java/com/lzb/memento/v1/Main.java
@@ -0,0 +1,31 @@
+package com.lzb.memento.v1;
+
+import java.util.Scanner;
+
+/**
+ * 备忘录模式<br/>
+ * Created on : 2021-10-13 12:41
+ *
+ * @author lizebin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        InputText inputText = new InputText();
+        SnapshotHolder snapshotHolder = new SnapshotHolder();
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String input = scanner.next();
+            if (":list".equals(input)) {
+                System.out.println(inputText.toString());
+            } else if (":undo".equals(input)) {
+                inputText.setText(snapshotHolder.popSnapshot().getText());
+            } else {
+                snapshotHolder.pushSnapshot(inputText);
+                inputText.append(input);
+            }
+        }
+    }
+
+}
