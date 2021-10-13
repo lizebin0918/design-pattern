6b9f5ab885b6caa4a6f185e7df69b3b4cf6d7f71
lizebin <lizebin0918@gmail.com>
2021-10-13 22:19:56 +0800

    feat:快照模式

---
+++ src/main/java/com/lzb/memento/v2/InputText.java
@@ -0,0 +1,29 @@
+package com.lzb.memento.v2;
+
+/**
+ * <br/>
+ * Created on : 2021-10-13 22:05
+ *
+ * @author lizebin
+ */
+public class InputText {
+
+    private StringBuilder text = new StringBuilder();
+
+    public String getText() {
+        return text.toString();
+    }
+
+    public void append(String input) {
+        text.append(input);
+    }
+
+    public Snapshot createSnapshot() {
+        return new Snapshot(text.toString());
+    }
+
+    public void restoreSnapshot(Snapshot snapshot) {
+        this.text.replace(0, this.text.length(), snapshot.getText());
+    }
+
+}
