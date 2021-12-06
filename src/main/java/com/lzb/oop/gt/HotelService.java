2a9e3905340f348a24d3faedf90b8a71669c48a1
lizebin <lizebin0918@gmail.com>
2021-12-06 23:18:09 +0800

    oo

---
+++ src/main/java/com/lzb/oop/gt/HotelService.java
@@ -0,0 +1,71 @@
+package com.lzb.oop.gt;
+
+import lombok.Data;
+
+/**
+ * 旅馆服务:假如我们正在开发一个酒店预订系统，针对不同的用户，我们需要计算出不同的房价。
+ * 比如，普通用户是全价，金卡是 8 折，银卡是 9 折，代码写出来可能是这样的<br/>
+ * Created on : 2021-12-06 22:57
+ *
+ * @author lizebin
+ */
+public class HotelService {
+
+    /**
+     * 获取房间价格
+     * @param user
+     * @param room
+     * @return
+     */
+    public double getRoomPriceV1(final User user, final Room room) {
+        double price = room.getPrice();
+        if (user.getLevel() == UserLevel.GOLD) {
+            return price * 0.8;
+        }
+        if (user.getLevel() == UserLevel.SILVER) {
+            return price * 0.9;
+        }
+        if (user.getLevel() == UserLevel.PLATINUM) {
+            return price * 0.75;
+        }
+        return price;
+    }
+
+
+    public double getRoomPriceV2(final User user, final Room room) {
+        return user.getRoomPrice(room);
+    }
+
+    private enum UserLevel {
+        /**
+         * 黄金、银、白金
+         */
+        GOLD(0.8), SILVER(0.9), PLATINUM(0.75);
+        private final double discount;
+        UserLevel(double price) {
+            this.discount = price;
+        }
+
+        public double getRoomPrice(final Room room) {
+            return discount * room.getPrice();
+        }
+    }
+
+    @Data
+    private static class User {
+        private String name;
+        private UserLevel level;
+
+        public double getRoomPrice(final Room room) {
+            return level.getRoomPrice(room);
+        }
+
+    }
+
+    @Data
+    private static class Room {
+        private double price;
+    }
+
+
+}
---
+++ src/main/java/com/lzb/oop/gt/ReportService.java
@@ -0,0 +1,77 @@
+package com.lzb.oop.gt;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 下面是一个常见的报表服务，首先我们取出当天的订单，然后生成订单的统计报表，还要把统计结果发送给相关的人等：
+ * 有一个新需求，把统计信息发给另外一个内部系统，这个内部系统可以把统计信息展示出来，供外部合作伙伴查阅。该怎么做呢？
+ */
+class ReportService {
+
+    public void processV1() {
+        // 获取当天的订单
+        List<Order> orders = fetchDailyOrders();
+        // 生成统计信息
+        OrderStatistics statistics = generateOrderStatistics(orders);
+
+        /*--------------------统计信息会分发到不同的系统-----------------*/
+        // 生成统计报表
+        generateStatisticsReport(statistics);
+        // 发送统计邮件
+        sendStatisticsByMail(statistics);
+    }
+
+    private List<OrderStatisticsConsumer> consumers;
+    public void processV2() {
+        // 获取当天的订单
+        List<Order> orders = fetchDailyOrders();
+        // 生成统计信息
+        OrderStatistics statistics = generateOrderStatistics(orders);
+
+        // 后两步和即将添加的步骤有一个共同点，都使用了统计信息，这样我们就找到了它们的共性，所以，我们就可以用一个共同的模型去涵盖它们
+        for (OrderStatisticsConsumer consumer : consumers) {
+            consumer.consume(statistics);
+        }
+    }
+
+    private void sendStatisticsByMail(OrderStatistics statistics) {
+
+    }
+
+    private void generateStatisticsReport(OrderStatistics statistics) {
+
+    }
+
+    private OrderStatistics generateOrderStatistics(List<Order> orders) {
+        return null;
+    }
+
+    private List<Order> fetchDailyOrders() {
+        return Collections.emptyList();
+    }
+
+    private static class Order {
+
+    }
+
+    private static class OrderStatistics {
+
+    }
+
+    private interface OrderStatisticsConsumer {
+        void consume(OrderStatistics statistics);
+    }
+
+    class StatisticsReporter implements OrderStatisticsConsumer {
+        public void consume(OrderStatistics statistics) {
+            generateStatisticsReport(statistics);
+        }
+    }
+
+    class StatisticsByMailer implements OrderStatisticsConsumer {
+        public void consume(OrderStatistics statistics) {
+            sendStatisticsByMail(statistics);
+        }
+    }
+}
