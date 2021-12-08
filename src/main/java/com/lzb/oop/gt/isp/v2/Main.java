bcd9cb511f34e09538c82c87d4598eed50bbb85b
lizebin <lizebin0918@gmail.com>
2021-12-08 23:53:45 +0800

    test:不会写Main....

--- src/main/java/com/lzb/oop/gt/isp/v2/Main.java
+++ src/main/java/com/lzb/oop/gt/isp/v2/Main.java
@@ -1,11 +1,14 @@
 package com.lzb.oop.gt.isp.v2;
 
+import com.lzb.oop.gt.isp.v1.TransactionType;
 import com.lzb.oop.gt.isp.v2.handler.DepositHandler;
 import com.lzb.oop.gt.isp.v2.handler.TransactionHandler;
 import com.lzb.oop.gt.isp.v2.request.ActualTransactionRequest;
+import com.lzb.oop.gt.isp.v2.request.DepositRequest;
+import com.lzb.oop.gt.isp.v2.request.TransactionRequest;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <br/>
@@ -17,9 +20,13 @@ public class Main {
 
     public static void main(String[] args) {
         ActualTransactionRequest request = new ActualTransactionRequest();
-        List<TransactionHandler> handlers = new ArrayList<>();
-        handlers.add(new DepositHandler());
-        handlers.forEach(item -> item.handle(request));
+        TransactionType type = TransactionType.TRANSFER;
+        Map<TransactionType, TransactionHandler<? extends TransactionRequest>> handlers = new HashMap<>();
+        TransactionHandler<DepositRequest> deposit = new DepositHandler();
+        handlers.put(TransactionType.DEPOSIT, deposit);
+
+        handlers.get(type).handle(request);
+
     }
 
 }
