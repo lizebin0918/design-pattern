090c85c9b23579a649c04e37b76e84a3604fe848
lizebin <bin.ze@shopcider.com>
2022-04-04 16:42:21 +0800

    feat:桥接模式

---
+++ src/main/java/com/lzb/bridge/yanmoshejimoshi/v1/Main.java
@@ -0,0 +1,15 @@
+package com.lzb.bridge.yanmoshejimoshi.v1;
+
+/**
+ * 实现发送消息
+ * 1.消息类型：普通、加急、特急
+ * 2.发送消息方式：邮件、短信、电话
+ *
+ * <br/>
+ * Created on : 2022-04-04 16:37
+ *
+ * @author lizebin
+ */
+public class Main {
+
+}
---
+++ src/main/java/com/lzb/bridge/yanmoshejimoshi/v1/message/Message.java
@@ -0,0 +1,13 @@
+package com.lzb.bridge.yanmoshejimoshi.v1.message;
+
+/**
+ * <br/>
+ * Created on : 2022-04-04 16:41
+ *
+ * @author lizebin
+ */
+public interface Message {
+
+    void send(String message, String toUser);
+
+}
