3203fa72976b75d19dd86a8aa1ac134614285d9c
lizebin <lizebin0918@gmail.com>
2022-05-08 19:58:33 +0800

    feat:重构类型

--- src/main/java/com/lzb/refactor_pratice/cart/Cart.java
+++ src/main/java/com/lzb/refactor_pratice/cart/Cart.java
@@ -5,27 +5,21 @@ package com.lzb.refactor_pratice.cart;
  * @author lizebin
  */
 public class Cart {
+
+    private final BlackCat blackCat = new BlackCat();
+    private final Hsinchu hsinchu = new Hsinchu();
+    private final PostOffice postOffice = new PostOffice();
+
     public double shippingFee(String shipper, Product product) {
         if (shipper.equals("black cat")) {
-            if (product.getWeight() > 20) {
-                return 500;
-            } else {
-                return 100 + product.getWeight() * 10;
-            }
+            return blackCat.calculate(product);
         } else if (shipper.equals("hsinchu")) {
-            double size = product.getLength() * product.getWidth() * product.getHeight();
-            if (product.getLength() > 100 || product.getWidth() > 100 || product.getHeight() > 100) {
-                return size * 0.00002 * 1100 + 500;
-            } else {
-                return size * 0.00002 * 1200;
-            }
+            return hsinchu.calculate(product);
         } else if (shipper.equals("post office")) {
-            double feeByWeight = 80 + product.getWeight() * 10;
-            double size = product.getLength() * product.getWidth() * product.getHeight();
-            double feeBySize = size * 0.00002 * 1100;
-            return Math.min(feeByWeight, feeBySize);
+            return postOffice.calculate(product);
         } else {
             throw new IllegalArgumentException("shipper not exist");
         }
     }
+
 }
---
+++ src/main/java/com/lzb/refactor_pratice/cart/ShippingFee.java
@@ -0,0 +1,17 @@
+package com.lzb.refactor_pratice.cart;
+
+public interface ShippingFee {
+
+    default double getSize(Product product) {
+        return product.getLength() * product.getWidth() * product.getHeight();
+    }
+
+    /**
+     * 计算运费
+     *
+     * @param product
+     * @return
+     */
+    double calculate(Product product);
+
+}
