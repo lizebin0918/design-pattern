8d021db7ba227d2a315449514e38096148ca8052
lizebin <lizebin0918@gmail.com>
2022-05-21 23:25:23 +0800

    feat:就这 ？

--- src/main/java/com/lzb/vistor/lzb/Main.java
+++ src/main/java/com/lzb/vistor/lzb/Main.java
@@ -2,7 +2,9 @@ package com.lzb.vistor.lzb;
 
 import lombok.Data;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 优惠券聚合根判断订单是否购买蔬菜，才赠送优惠券<br/>
@@ -13,7 +15,14 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
+        ProductGateway productGateway = new ProductGateway();
+        Order order = new Order();
+        order.setOrderItems(Arrays.asList(new OrderItem(1, 2, 3)));
+        if (order.accept(productGateway::hasVegatable)) {
+            System.out.println("包含蔬菜");
+        } else {
+            System.out.println("不包含蔬菜");
+        }
     }
 
     @Data
@@ -22,7 +31,7 @@ public class Main {
         private long orderId;
         private List<OrderItem> orderItems;
 
-        <T> T accept(OrderVisitor<T> visitor) {
+        public <T> T accept(OrderVisitor<T> visitor) {
             return visitor.visit(orderItems);
         }
 
@@ -36,19 +45,19 @@ public class Main {
     private static class OrderItem {
         private long id;
         private long orderId;
-        private String productName;
+        private long productId;
+
+        public OrderItem(long id, long orderId, long productId) {
+            this.id = id;
+            this.orderId = orderId;
+            this.productId = productId;
+        }
     }
 
-    @Data
-    private static class Coupon {
-
-        private long couponId;
-
-        public void giveCoupon() {
-
+    private static class ProductGateway {
+        public boolean hasVegatable(List<OrderItem> items) {
+            return items.stream().map(OrderItem::getProductId).collect(Collectors.toList()).contains(1L);
         }
-
-
     }
 
 }
