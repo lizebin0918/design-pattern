b4c482aee7d01b034637309f255a53d4b53f48b0
lizebin <bin.ze@shopcider.com>
2022-05-31 11:50:05 +0800

    feat:初始化提交，需要添加：UNIX_REQUESTED

---
+++ src/main/java/com/lzb/refactor_pratice/state_permission/SystemPermission.java
@@ -0,0 +1,68 @@
+package com.lzb.refactor_pratice.state_permission;
+
+import lombok.Data;
+
+@Data
+public class SystemPermission {
+    private SystemProfile profile;
+    private SystemUser requestor; 
+    private SystemAdmin admin; 
+    private boolean isGranted; 
+    private String state; 
+    public final static String REQUESTED = "REQUESTED"; 
+    public final static String CLAIMED = "CLAIMED";
+    public final static String GRANTED = "GRANTED"; 
+    public final static String DENIED = "DENIED"; 
+    
+    public SystemPermission(SystemUser requestor, SystemProfile profile) { 
+        this.requestor = requestor; 
+        this.profile = profile; 
+        state = REQUESTED; 
+        isGranted = false; 
+        notifyAdminOfPermissionRequest(); 
+    }
+
+    private void notifyAdminOfPermissionRequest() {
+        System.out.println("notifyAdminOfPermissionRequest");
+    }
+
+    public void claimedBy(SystemAdmin admin) { 
+        if (!state.equals(REQUESTED)) 
+            return; 
+            
+        willBeHandledBy(admin); 
+        state = CLAIMED; 
+    }
+
+    private void willBeHandledBy(SystemAdmin admin) {
+        System.out.println("willBeHandledBy");
+    }
+
+    public void deniedBy(SystemAdmin admin) { 
+        if (!state.equals(CLAIMED)) 
+            return; 
+            
+        if (!this.admin.equals(admin)) 
+            return; 
+        
+        isGranted = false; 
+        state = DENIED; 
+        notifyUserOfPermissionRequestResult(); 
+    }
+
+    private void notifyUserOfPermissionRequestResult() {
+        System.out.println("notifyUserOfPermissionRequestResult");
+    }
+
+    public void grantedBy(SystemAdmin admin) { 
+        if (!state.equals(CLAIMED)) 
+            return; 
+            
+        if (!this.admin.equals(admin)) 
+            return; 
+            
+        state = GRANTED; 
+        isGranted = true; 
+        notifyUserOfPermissionRequestResult(); 
+    }
+}
