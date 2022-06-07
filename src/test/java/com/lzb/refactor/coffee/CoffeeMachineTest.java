d06a4127a9da3b922fa7274d0e027c13e9ea00a0
lizebin <bin.ze@shopcider.com>
2022-06-07 09:51:02 +0800

    feat:建立安全网

--- src/test/java/com/lzb/refactor/coffee/CoffeeMachineTest.java
+++ src/test/java/com/lzb/refactor/coffee/CoffeeMachineTest.java
@@ -4,12 +4,9 @@ import com.lzb.SystemOutUtils;
 import org.approvaltests.ApprovalUtilities;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.ByteArrayOutputStream;
 
-@RunWith(MockitoJUnitRunner.class)
 class CoffeeMachineTest {
 
     @Test
@@ -24,11 +21,13 @@ class CoffeeMachineTest {
         Approvals.verify(SystemOutUtils.convert(output));
     }
 
-    /*@Test
+    @Test
     void test_two() {
         ByteArrayOutputStream output = new ApprovalUtilities().writeSystemOutToStringBuffer();
-        CoffeeMachineMock.run("2");
+        CoffeeMachineMock.run("1");
+        CoffeeMachineMock.run("1");
+        CoffeeMachineMock.run("1");
         Approvals.verify(SystemOutUtils.convert(output));
-    }*/
+    }
 
 }
