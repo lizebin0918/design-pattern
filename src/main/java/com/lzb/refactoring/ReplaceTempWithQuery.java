e12a263146968d9e5e2a2a665e3cae1b860acb4f
lizebin <lizebin0918@gmail.com>
2022-07-08 00:06:49 +0800

    feat:重构练习

---
+++ src/main/java/com/lzb/refactoring/ReplaceTempWithQuery.java
@@ -0,0 +1,54 @@
+package com.lzb.refactoring;
+
+/**
+ * 放大了"basePrice"的作用域<br/>
+ * Created on : 2022-07-07 23:46
+ *
+ * @author lizebin
+ */
+public class ReplaceTempWithQuery {
+
+    private double quantity, itemPrice;
+
+    double calculateTotal() {
+        double basePrice = quantity * itemPrice;
+        if (basePrice > 1000) {
+            return basePrice * 0.95;
+        } else {
+            return basePrice * 0.98;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    ///////////////////////////////////////////////////////////////////////////
+
+    double calculateTotal1() {
+        if (basePrice() > 1000) {
+            return basePrice() * 0.95;
+        } else {
+            return basePrice() * 0.98;
+        }
+    }
+
+    double calculateTotal2() {
+        double basePrice = basePrice();
+        double factor = getFactor(basePrice);
+        return basePrice * factor;
+    }
+
+    private double getFactor(double basePrice) {
+        double factor;
+        if (basePrice > 1000) {
+            factor = 0.95;
+        } else {
+            factor = 0.98;
+        }
+        return factor;
+    }
+
+    double basePrice() {
+        return quantity * itemPrice;
+    }
+
+}
