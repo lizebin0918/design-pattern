6837e0c7c130d5d9787a518d5d77291dbdd1c751
lizebin <lizebin0918@gmail.com>
2023-12-12 19:45:54 +0800

    feat:遍历不同类型的文件

--- src/main/java/com/lzb/vistor/ruanjiansheji/Main.java
+++ src/main/java/com/lzb/vistor/ruanjiansheji/Main.java
@@ -1,9 +1,8 @@
 package com.lzb.vistor.ruanjiansheji;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
-
-import lombok.AllArgsConstructor;
 
 /**
  * 遍历文件，分别打印txt和java后缀的文件名。例子来自：《从专业到卓越》<br/>
@@ -12,22 +11,22 @@ import lombok.AllArgsConstructor;
  */
 public class Main {
 
-    @AllArgsConstructor(staticName = "of")
     private static class FileTraversal {
 
-        private final List<FileVisitor> visitors;
-        private final File file;
+        private final List<File> files = new ArrayList<>();
 
-        public void traverse() {
-            if (file.isDirectory()) {
-                for (File child : file.listFiles()) {
-                    FileTraversal.of(visitors, child).traverse();
+        public void traverse(File sourceFile) {
+            if (sourceFile.isDirectory()) {
+                for (File child : sourceFile.listFiles()) {
+                    traverse(child);
                 }
                 return;
             }
-            for (FileVisitor visitor : visitors) {
-                visitor.visit(file);
-            }
+            files.add(sourceFile);
+        }
+
+        public void accept(FileVisitor visitor) {
+            files.forEach(visitor::visit);
         }
 
     }
@@ -38,19 +37,24 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        FileTraversal.of(List.of(
-                (file) -> {
-                    if (file.getName().endsWith(".txt")) {
-                        System.out.println("txt:" + file.getName());
-                    }
+        FileTraversal fileTraversal = new FileTraversal();
+        fileTraversal.traverse(new File("/Users/lizebin/project/my-ddd"));
+        List<FileVisitor> visitors = new ArrayList<>();
+        visitors.add(
+            file -> {
+                if (file.getName().endsWith(".txt")) {
+                    System.out.println("txt:" + file.getName());
                 }
-                ,
-                (file) -> {
-                    if (file.getName().endsWith(".java")) {
-                        System.out.println("java:" + file.getName());
-                    }
+            }
+        );
+        visitors.add(
+            file -> {
+                if (file.getName().endsWith(".java")) {
+                    System.out.println("java:" + file.getName());
                 }
-        ), new File("/Users/lizebin/project/my-ddd")).traverse();
+            }
+        );
+        visitors.forEach(fileTraversal::accept);
     }
 
 }
